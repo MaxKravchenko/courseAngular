1579b89cc876ff9beacb6c8ce55a87f383045bce
max.kravchenko.dev@gmail.com <Maxim Kravchenko>
2023-12-18 09:43:21 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NotesListComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'photoEditor';
+  isRed = false;
+  products = ['apple', 'orange', 'banana'];
+  loggedIn = true;
+  name = 'User Name';
+  password = 'User Password';
+  
+  login() {
+    alert('Welcome!');
+  }
+  
+  change() {
+    this.isRed = !this.isRed;
+  }
+
+  showName() {
+    alert(this.name);
+  }
 }
